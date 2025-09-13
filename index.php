26a145fe64047d3ef48ec15094e1c270ec025aea
barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2025-09-13 19:16:52 +1000

    first and last

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My PHP Web App</title>
+    <style>
+        body { font-family: Arial, sans-serif; text-align: center; margin-top: 50px; }
+        h1 { color: #4CAF50; }
+        p {
+            font-size: 24px;
+       
+        }
+    </style>
+</head>
+<body>
+    <h1>PHP Web App</h1>
+    <p>This is SWE 40006</p>
+    <P>Software Deployment and Evolution</P>
+    <P>D/HD Task</P>
+</body>
+</html>
